4eb2e77f1f98081f7427c4f198c07fcdd7d733d7
PavelBartenev <56157750+PavelBartenev@users.noreply.github.com>
2020-04-18 14:32:23 +0300

    Add files via upload

---
+++ Asm_hack.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	FILE* output = fopen("W://hack.txt", "wb");
+
+	int first_byte = 1;
+	//29
+	int last_byte = 19;
+	int zero = 0;
+	int enter = 13;
+	//65175
+
+	//65152
+	for (int i = 0; i < 65152; ++i)
+	{
+		fwrite(&zero, 1, 1, output);
+	}
+	
+	fwrite(&last_byte, 1, 1, output);
+	fwrite(&first_byte, 1, 1, output);
+
+    fwrite(&enter, 1, 1, output);
+
+	return 0;
+}
